1dea0bff4f2c92acbb37e403efeb53d65b0979c1
Fabrizio Di Marco <fabrizio.dimarco@gmail.com>
2024-09-19 11:32:05 +0200

    commit

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ async function getApiData() {
     console.log("lon: " + longitude);
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,rain&daily=sunrise,sunset`;
     const options = {
-        method: 'GET',
-        // headers: {
-        //     'x-rapidapi-key': "961c8493b3msh7791f7995ea79d5p181750jsn32726307c3d7",
-        //     'x-rapidapi-host': 'meteostat.p.rapidapi.com'
-        // }
+        method: 'GET'
     };
 
     try {
